29e5226754f93454b7b138763358b10c03812ee5
Sean Johnson <sean.johnson.arizona@gmail.com>
2024-09-27 14:27:21 -0700

    Created IdempotenceID and PartitionID.

--- modules/eventsource/command.go
+++ modules/eventsource/command.go
@@ -3,4 +3,6 @@ package eventsource
 type (
 	CorrelationID string
 	UserID        string
+	IdempotenceID string
+	PartitionID   string
 )
--- modules/eventsource/event.go
+++ modules/eventsource/event.go
@@ -17,38 +17,19 @@ type (
 	Version       int
 
 	Event struct {
-		// The global version of the Event
-		GlobalVersion GlobalVersion
-
-		// The name of the Aggregate
-		AggregateName AggregateName
-
-		// The unique ID of the Event
-		ID EventID
-
-		// The ID of the Aggregate to which this Event applies
-		AggregateID AggregateID
-
-		// The Version of the Aggregate
-		Version Version
-
-		// The unique GetName of the Event
-		Name EventName
-
-		// The ID of the entire Command and Event chain
-		CorrelationID CorrelationID
-
-		// The ID of the User issuing the Command
-		UserID UserID
-
-		// The Time this Event was created
-		Time time.Time
-
-		// The schema for the internal data
-		Schema Schema
-
-		// The internal data of the Event
-		Data []byte
+		GlobalVersion GlobalVersion // The global version of the Event
+		AggregateName AggregateName // The name of the Aggregate
+		ID            EventID       // The unique ID of the Event
+		AggregateID   AggregateID   // The ID of the Aggregate to which this Event applies
+		Version       Version       // The Version of the Aggregate
+		Name          EventName     // The unique GetName of the Event
+		CorrelationID CorrelationID // The ID of the entire Command and Event chain
+		IdempotenceID IdempotenceID // The ID to de-duplicate Events
+		PartitionID   PartitionID   // The ID to partition Events
+		UserID        UserID        // The ID of the User issuing the Command
+		Time          time.Time     // The Time this Event was created
+		Schema        Schema        // The schema for the internal data
+		Data          []byte        // The internal data of the Event
 	}
 
 	EventData interface {
