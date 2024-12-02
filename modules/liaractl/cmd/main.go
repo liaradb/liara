bd74714ab246e6b22c2d16c41df36153a210c3a3
Sean Johnson <sean.johnson.arizona@gmail.com>
2024-12-02 13:26:14 -0700

    Created liaractl commands.

--- modules/liaractl/cmd/main.go
+++ modules/liaractl/cmd/main.go
@@ -2,15 +2,39 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/cardboardrobots/liara/esgrpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+type object string
+type command string
+
+const (
+	objectTenant object  = "tenant"
+	objectEvent  object  = "event"
+	commandList  command = "list"
+)
+
 func main() {
+	cmd := getArgs()
+	switch cmd {
+	case objectTenant:
+		listTenants()
+	case objectEvent:
+		listEvents()
+	default:
+		fmt.Println("no command")
+		break
+	}
+}
+
+func listTenants() {
 	grpcConn, err := grpc.NewClient("localhost:50055",
 		grpc.WithTransportCredentials(
 			insecure.NewCredentials()))
@@ -23,9 +47,47 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		count++
 	}
 	if count == 0 {
 		fmt.Println("no tenants")
+	}
+}
+
+func listEvents() {
+	grpcConn, err := grpc.NewClient("localhost:50055",
+		grpc.WithTransportCredentials(
+			insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	es := esgrpc.NewEventSourceGRPC(grpcConn)
+	count := 0
+	for event, err := range es.GetAfterGlobalVersion(context.Background(), 0, nil, 0) {
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var data = make(map[string]any)
+		_ = json.Unmarshal(event.Data, &data)
+
+		log.Printf("%v\n", data)
+		count++
+	}
+	if count == 0 {
+		fmt.Println("no events")
+	}
+}
+
+func getArgs() object {
+	args := os.Args
+	switch len(args) {
+	case 0:
+		fallthrough
+	case 1:
+		return ""
+	default:
+		return object(args[1])
 	}
 }
