275dc10383b92f7035c51c9b1ae7e4e6af9f2b83
Sean Johnson <sean.johnson.arizona@gmail.com>
2024-12-01 23:43:08 -0700

    Created RequestRepository methods.

--- services/liara_service/feature/eventsource/infrastructure/request_repository.go
+++ services/liara_service/feature/eventsource/infrastructure/request_repository.go
@@ -3,8 +3,10 @@ package infrastructure
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"time"
 
+	"github.com/cardboardrobots/liara_service/feature/eventsource/domain/entity"
 	"github.com/cardboardrobots/liara_service/feature/eventsource/domain/service"
 	"github.com/cardboardrobots/liara_service/feature/eventsource/domain/value"
 )
@@ -30,14 +32,64 @@ func NewRequestRepository(db *sql.DB, name string) *RequestRepository {
 	}
 }
 
-func (r *RequestRepository) Insert(context.Context, value.RequestID, time.Time) error {
-	return nil
+func (r *RequestRepository) Insert(ctx context.Context, requestID value.RequestID, time time.Time) error {
+	return r.insertRow(ctx, r.db, entity.RequestLog{ID: requestID, Time: time})
 }
 
-func (r *RequestRepository) Purge(context.Context, time.Time) error {
-	return nil
+func (r *RequestRepository) Purge(ctx context.Context, time time.Time) error {
+	_, err := r.db.ExecContext(ctx, fmt.Sprintf(`
+DELETE FROM %v WHERE
+time <= $1
+`,
+		r.name), time)
+	return err
 }
 
-func (r *RequestRepository) Test(context.Context, value.RequestID) error {
-	return nil
+func (r *RequestRepository) Test(ctx context.Context, requestID value.RequestID) error {
+	row := r.db.QueryRowContext(ctx, fmt.Sprintf(`
+SELECT * FROM %v WHERE
+id = $1
+`,
+		r.name), requestID)
+	_, err := r.scanRow(row)
+	if err == sql.ErrNoRows {
+		err = nil
+	}
+	return err
+}
+
+func (r RequestRepository) insertRow(
+	ctx context.Context,
+	qr queryRunner,
+	request entity.RequestLog,
+) error {
+	_, err := qr.ExecContext(ctx, fmt.Sprintf(`
+INSERT INTO %v VALUES( $1, $2 )
+`,
+		r.name),
+		request.ID,
+		request.Time,
+	)
+	return err
+}
+
+func (r RequestRepository) scanRow(row Row) (entity.RequestLog, error) {
+	request := entity.RequestLog{}
+	err := row.Scan(
+		&request.ID,
+		&request.Time,
+	)
+	return request, err
+}
+
+func (r *RequestRepository) CreateTable(ctx context.Context) error {
+	query := fmt.Sprintf(`
+CREATE TABLE IF NOT EXISTS %v (
+	id VARCHAR(50) PRIMARY KEY NOT NULL,
+	time TIMESTAMP NOT NULL,
+);
+`,
+		r.name)
+	_, err := r.db.ExecContext(ctx, query)
+	return err
 }
