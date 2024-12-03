b0a166d3ef74cd3063cb885c976535f54c017f2e
Sean Johnson <sean.johnson.arizona@gmail.com>
2024-12-03 14:15:06 -0700

    Fixed event formatting.

--- modules/liaractl/handler/event_handler.go
+++ modules/liaractl/handler/event_handler.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"time"
 
+	"github.com/cardboardrobots/liara"
 	"github.com/cardboardrobots/liara/esgrpc"
 )
 
@@ -28,9 +30,7 @@ func (eh *eventHandler) listEvents() error {
 			return err
 		}
 
-		var data = make(map[string]any)
-		_ = json.Unmarshal(event.Data, &data)
-		result, _ := json.MarshalIndent(data, "", "    ")
+		result, _ := json.MarshalIndent(eventToRecord(event), "", "    ")
 
 		fmt.Printf("%v,\n", string(result))
 		count++
@@ -39,4 +39,41 @@ func (eh *eventHandler) listEvents() error {
 		fmt.Println("no events")
 	}
 	return nil
+}
+
+func eventToRecord(event liara.Event) Event {
+	var data = make(map[string]any)
+	_ = json.Unmarshal(event.Data, &data)
+
+	return Event{
+		GlobalVersion: event.GlobalVersion,
+		ID:            event.ID,
+		AggregateName: event.AggregateName,
+		AggregateID:   event.AggregateID,
+		Version:       event.Version,
+		PartitionID:   event.PartitionID,
+		Name:          event.Name,
+		Schema:        event.Schema,
+		Metadata:      EventMetadata(event.Metadata),
+		Data:          data,
+	}
+}
+
+type Event struct {
+	GlobalVersion liara.GlobalVersion `json:"globalVersion"`
+	ID            liara.EventID       `json:"id"`
+	AggregateName liara.AggregateName `json:"aggregateName"`
+	AggregateID   liara.AggregateID   `json:"aggregateId"`
+	Version       liara.Version       `json:"version"`
+	PartitionID   liara.PartitionID   `json:"partitionId"`
+	Name          liara.EventName     `json:"name"`
+	Schema        liara.Schema        `json:"schema"`
+	Metadata      EventMetadata       `json:"metadata"`
+	Data          any                 `json:"data"`
+}
+
+type EventMetadata struct {
+	UserID        liara.UserID        `json:"userId"`
+	CorrelationID liara.CorrelationID `json:"correlationId"`
+	Time          time.Time           `json:"time"`
 }
