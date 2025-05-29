6a84b79d9339c79ad9fb3978cbe643ecbf35fc94
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-05-29 09:44:07 -0700

    Cleaned iterator functions.

--- services/liara_service/feature/base/iter_error.go
+++ services/liara_service/feature/base/iter_error.go
@@ -6,6 +6,5 @@ func IterError[T any](err error) iter.Seq2[T, error] {
 	return func(yield func(T, error) bool) {
 		var t T
 		yield(t, err)
-		return
 	}
 }
--- services/liara_service/feature/base/iter_to_slice.go
+++ services/liara_service/feature/base/iter_to_slice.go
@@ -2,14 +2,14 @@ package base
 
 import "iter"
 
-func IterToSlice[T any](i iter.Seq2[T, error]) ([]T, int, error) {
+func IterToSlice[T any](i iter.Seq2[T, error]) ([]T, error) {
 	s := make([]T, 0)
 	for t, err := range i {
 		if err != nil {
-			return nil, 0, err
+			return nil, err
 		}
 
 		s = append(s, t)
 	}
-	return s, 0, nil
+	return s, nil
 }
