8582ffe53e89b71491c4de54a53c49bc58173176
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-08-26 14:19:27 -0700

    Cleaned SharedPool test.

--- services/liaradb/sharedpool/shared_pool_test.go
+++ services/liaradb/sharedpool/shared_pool_test.go
@@ -9,40 +9,41 @@ import (
 
 func TestSharedPool(t *testing.T) {
 	t.Parallel()
+	synctest.Test(t, testSharedPool)
+}
 
-	synctest.Test(t, func(t *testing.T) {
-		sp := NewSharedPool[string, *testItem](2)
-		defer sp.Close()
+func testSharedPool(t *testing.T) {
+	sp := NewSharedPool[string, *testItem](2)
+	defer sp.Close()
 
-		for i := range 2 {
-			sp.Add(&testItem{id: i})
-		}
+	for i := range 2 {
+		sp.Add(&testItem{id: i})
+	}
 
-		ctx := context.Background()
+	ctx := context.Background()
 
-		if a, ok := sp.Request(ctx, "a"); !ok || a == nil {
-			t.Error("should get value")
-		}
+	if a, ok := sp.Request(ctx, "a"); !ok || a == nil {
+		t.Error("should get value")
+	}
 
-		var b *testItem
-		var ok bool
-		if b, ok = sp.Request(ctx, "b"); !ok || b == nil {
-			t.Error("should get value")
-		}
+	var b *testItem
+	var ok bool
+	if b, ok = sp.Request(ctx, "b"); !ok || b == nil {
+		t.Error("should get value")
+	}
 
-		ctx2, cancel := context.WithTimeout(ctx, 1*time.Second)
-		defer cancel()
+	ctx2, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
 
-		if c, ok := sp.Request(ctx2, "c"); ok || c != nil {
-			t.Error("should not get value")
-		}
+	if c, ok := sp.Request(ctx2, "c"); ok || c != nil {
+		t.Error("should not get value")
+	}
 
-		sp.Release(b)
+	sp.Release(b)
 
-		if c, ok := sp.Request(ctx, "c"); !ok || c == nil {
-			t.Error("should get value")
-		}
-	})
+	if c, ok := sp.Request(ctx, "c"); !ok || c == nil {
+		t.Error("should get value")
+	}
 }
 
 type testItem struct {
