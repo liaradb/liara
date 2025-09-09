8086154ebf76aa903e995a47ead26440ee4cb74d
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-09-09 13:30:03 -0700

    Created CRC.

---
+++ services/liaradb/log/crc.go
@@ -0,0 +1,15 @@
+package log
+
+import "hash/crc32"
+
+type CRC uint32
+
+var table = crc32.MakeTable(crc32.Koopman)
+
+func NewCRC(data []byte) CRC {
+	return CRC(crc32.Checksum(data, table))
+}
+
+func (c CRC) Compare(data []byte) bool {
+	return NewCRC(data) == c
+}
