d42722c84738bd3c17794b44b94ef5655a704b77
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-09-12 12:04:55 -0700

    Created LogPageID serialization.

--- services/liaradb/log/log_page_id.go
+++ services/liaradb/log/log_page_id.go
@@ -1,3 +1,16 @@
 package log
 
+import (
+	"encoding/binary"
+	"io"
+)
+
 type LogPageID uint64
+
+func (lpid *LogPageID) Write(w io.Writer) error {
+	return binary.Write(w, binary.BigEndian, *lpid)
+}
+
+func (lpid *LogPageID) Read(r io.Reader) error {
+	return binary.Read(r, binary.BigEndian, lpid)
+}
---
+++ services/liaradb/log/log_page_id_test.go
@@ -0,0 +1,28 @@
+package log
+
+import (
+	"io"
+	"testing"
+)
+
+func TestLogPageIDTest(t *testing.T) {
+	r, w := createReaderWriter()
+
+	var lpid LogPageID = 123456
+	if err := lpid.Write(w); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := w.Flush(); err != nil {
+		t.Fatal(err)
+	}
+
+	var lpid2 LogPageID
+	if err := lpid2.Read(r); err != nil && err != io.EOF {
+		t.Fatal(err)
+	}
+
+	if lpid != lpid2 {
+		t.Errorf("incorrect value: %v, expected: %v", lpid2, lpid)
+	}
+}
