0e2df8e90694161228671347368b48e52c883cc1
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-09-11 20:06:06 -0700

    Created LogRecord Write and Read test.

--- services/liaradb/log/log_record_test.go
+++ services/liaradb/log/log_record_test.go
@@ -21,6 +21,35 @@ func TestLogRecord(t *testing.T) {
 	GetterArray(t, lr.Reverse, reverse, "Reverse")
 }
 
+func TestLogRecord_Write(t *testing.T) {
+	lsn := LogSequenceNumber(1)
+	tid := TransactionID(2)
+	data := []byte("abcde")
+	reverse := []byte("fghij")
+
+	lr := newLogRecord(lsn, tid, data, reverse)
+
+	r, w := createReaderWriter()
+
+	if err := lr.Write(w); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := w.Flush(); err != nil {
+		t.Fatal(err)
+	}
+
+	lr2 := &LogRecord{}
+	if err := lr2.Read(r); err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Getter(t, lr2.LogSequenceNumber, lsn, "LogSequenceNumber")
+	assert.Getter(t, lr2.TransactionID, tid, "TransactionID")
+	GetterArray(t, lr2.Data, data, "Data")
+	GetterArray(t, lr2.Reverse, reverse, "Reverse")
+}
+
 func GetterArray[T comparable](t *testing.T, a func() []T, b []T, name string) bool {
 	t.Helper()
 
