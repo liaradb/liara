e3a18b33b97e857b03ffd61f0e55e73dee2b028d
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-09-18 12:55:25 -0700

    Moved Segment list functions.

--- services/liaradb/log/segment.go
+++ services/liaradb/log/segment.go
@@ -1,10 +1,5 @@
 package log
 
-import (
-	"io/fs"
-	"slices"
-)
-
 type Segment struct {
 	size     int // number of pages
 	pageSize int // page size
@@ -19,46 +14,3 @@ func NewSegment(size int, pageSize int) *Segment {
 
 func (s *Segment) Size() int     { return s.size }
 func (s *Segment) PageSize() int { return s.pageSize }
-
-func GetLatestSegment(names []SegmentName) SegmentName {
-	if len(names) > 0 {
-		return names[len(names)-1]
-	}
-
-	return SegmentName{}
-}
-
-func GetSegmentForLSN(names []SegmentName, lsn LogSequenceNumber) (SegmentName, bool) {
-	for i := len(names) - 1; i >= 0; i-- {
-		n := names[i]
-		if lsn >= n.lsn {
-			return n, true
-		}
-	}
-
-	return SegmentName{}, false
-}
-
-type ReadDir interface {
-	ReadDir(name string) ([]fs.DirEntry, error)
-}
-
-func ListSegments(fsys ReadDir, dir string) ([]SegmentName, error) {
-	files, err := fsys.ReadDir(dir)
-	if err != nil {
-		return nil, err
-	}
-
-	names := make([]SegmentName, 0, len(files))
-	for _, f := range files {
-		if !f.IsDir() {
-			names = append(names, ParseSegmentName(f.Name()))
-		}
-	}
-
-	slices.SortFunc(names, func(a, b SegmentName) int {
-		return int(a.ID()) - int(b.ID())
-	})
-
-	return names, nil
-}
---
+++ services/liaradb/log/segment_list.go
@@ -0,0 +1,61 @@
+package log
+
+import (
+	"io/fs"
+	"slices"
+
+	"github.com/liaradb/liaradb/file"
+)
+
+func GetLatestSegment(names []SegmentName) SegmentName {
+	if len(names) > 0 {
+		return names[len(names)-1]
+	}
+
+	return SegmentName{}
+}
+
+func GetSegmentForLSN(names []SegmentName, lsn LogSequenceNumber) (SegmentName, bool) {
+	for i := len(names) - 1; i >= 0; i-- {
+		n := names[i]
+		if lsn >= n.lsn {
+			return n, true
+		}
+	}
+
+	return SegmentName{}, false
+}
+
+type ReadDir interface {
+	ReadDir(name string) ([]fs.DirEntry, error)
+}
+
+func ListSegments(fsys ReadDir, dir string) ([]SegmentName, error) {
+	files, err := fsys.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	names := make([]SegmentName, 0, len(files))
+	for _, f := range files {
+		if !f.IsDir() {
+			names = append(names, ParseSegmentName(f.Name()))
+		}
+	}
+
+	slices.SortFunc(names, func(a, b SegmentName) int {
+		return int(a.ID()) - int(b.ID())
+	})
+
+	return names, nil
+}
+
+func OpenLatestSegment(fsys file.FileSystem, dir string) (file.File, error) {
+	names, err := ListSegments(fsys, ".")
+	if err != nil {
+		return nil, err
+	}
+
+	sn := GetLatestSegment(names)
+	return fsys.Open(sn.String())
+}
