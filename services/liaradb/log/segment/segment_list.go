b67dd04966cfa67c3e8d6e7dc128375119e98858
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-09-20 17:33:49 -0700

    Sorted SegmentList fields and methods.

--- services/liaradb/log/segment/segment_list.go
+++ services/liaradb/log/segment/segment_list.go
@@ -9,20 +9,18 @@ import (
 )
 
 type SegmentList struct {
-	fsys  file.FileSystem
 	dir   string
-	names []SegmentName
 	file  file.File
+	fsys  file.FileSystem
+	names []SegmentName
 }
 
 func NewSegmentList(fsys file.FileSystem, dir string) *SegmentList {
 	return &SegmentList{
-		fsys: fsys,
 		dir:  dir,
+		fsys: fsys,
 	}
 }
-
-func (sl *SegmentList) Names() []SegmentName { return sl.names }
 
 func (sl *SegmentList) Close() error {
 	if sl.file == nil {
@@ -37,6 +35,8 @@ func (sl *SegmentList) Close() error {
 	return nil
 }
 
+func (sl *SegmentList) Names() []SegmentName { return sl.names }
+
 // TODO: Should not open files unless this is called
 func (sl *SegmentList) Open() error {
 	files, err := sl.getFiles()
@@ -47,49 +47,6 @@ func (sl *SegmentList) Open() error {
 	names := sl.filesToNames(files)
 	sl.names = names
 	return nil
-}
-
-func (sl *SegmentList) getFiles() ([]fs.DirEntry, error) {
-	return sl.fsys.ReadDir(sl.dir)
-}
-
-func (*SegmentList) filesToNames(files []fs.DirEntry) []SegmentName {
-	names := make([]SegmentName, 0, len(files))
-	for _, f := range files {
-		if !f.IsDir() {
-			names = append(names, ParseSegmentName(f.Name()))
-		}
-	}
-	// TODO: Do we need to sort?
-	slices.SortFunc(names, func(a, b SegmentName) int {
-		return int(a.ID() - b.ID())
-	})
-	return names
-}
-
-func (sl *SegmentList) OpenNextSegment(lsn record.LogSequenceNumber) (SegmentName, file.File, error) {
-	if err := sl.Close(); err != nil {
-		return SegmentName{}, nil, err
-	}
-
-	sn := sl.getNextSegment(lsn)
-	f, err := sl.fsys.OpenFile(sn.String())
-	if err != nil {
-		return SegmentName{}, nil, err
-	}
-
-	sl.file = f
-	sl.names = append(sl.names, sn)
-
-	return sn, f, err
-}
-
-func (sl *SegmentList) getNextSegment(lsn record.LogSequenceNumber) SegmentName {
-	if len(sl.names) > 0 {
-		return sl.names[len(sl.names)-1].Next(lsn)
-	}
-
-	return SegmentName{}
 }
 
 func (sl *SegmentList) OpenLatestSegment() (SegmentName, file.File, error) {
@@ -111,42 +68,21 @@ func (sl *SegmentList) OpenLatestSegment() (SegmentName, file.File, error) {
 	return sn, f, err
 }
 
-func (sl *SegmentList) getLatestSegment() (SegmentName, bool) {
-	if len(sl.names) > 0 {
-		return sl.names[len(sl.names)-1], true
-	}
-
-	return SegmentName{}, false
-}
-
-func (sl *SegmentList) OpenSegmentForLSN(lsn record.LogSequenceNumber) (SegmentName, file.File, error) {
+func (sl *SegmentList) OpenNextSegment(lsn record.LogSequenceNumber) (SegmentName, file.File, error) {
 	if err := sl.Close(); err != nil {
 		return SegmentName{}, nil, err
 	}
 
-	sn, ok := sl.getSegmentForLSN(lsn)
-	if !ok {
-		return SegmentName{}, nil, ErrNoSegmentFile
-	}
+	sn := sl.getNextSegment(lsn)
 	f, err := sl.fsys.OpenFile(sn.String())
 	if err != nil {
 		return SegmentName{}, nil, err
 	}
 
 	sl.file = f
+	sl.names = append(sl.names, sn)
 
 	return sn, f, err
-}
-
-func (sl *SegmentList) getSegmentForLSN(lsn record.LogSequenceNumber) (SegmentName, bool) {
-	for i := len(sl.names) - 1; i >= 0; i-- {
-		n := sl.names[i]
-		if lsn >= n.lsn {
-			return n, true
-		}
-	}
-
-	return SegmentName{}, false
 }
 
 func (sl *SegmentList) OpenSegmentBeforeLSN(lsn record.LogSequenceNumber) (SegmentName, file.File, error) {
@@ -168,6 +104,25 @@ func (sl *SegmentList) OpenSegmentBeforeLSN(lsn record.LogSequenceNumber) (Segme
 	return sn, f, nil
 }
 
+func (sl *SegmentList) OpenSegmentForLSN(lsn record.LogSequenceNumber) (SegmentName, file.File, error) {
+	if err := sl.Close(); err != nil {
+		return SegmentName{}, nil, err
+	}
+
+	sn, ok := sl.getSegmentForLSN(lsn)
+	if !ok {
+		return SegmentName{}, nil, ErrNoSegmentFile
+	}
+	f, err := sl.fsys.OpenFile(sn.String())
+	if err != nil {
+		return SegmentName{}, nil, err
+	}
+
+	sl.file = f
+
+	return sn, f, err
+}
+
 // TODO: Handle if this file is open
 func (sl *SegmentList) RemoveSegmentBeforeLSN(lsn record.LogSequenceNumber) error {
 	sn, index, ok := sl.getSegmentBeforeLSN(lsn)
@@ -180,13 +135,22 @@ func (sl *SegmentList) RemoveSegmentBeforeLSN(lsn record.LogSequenceNumber) erro
 	return sl.fsys.Remove(sn.String())
 }
 
-func (sl *SegmentList) getSegmentBeforeLSN(lsn record.LogSequenceNumber) (SegmentName, int, bool) {
-	index := sl.getIndexForLSN(lsn)
-	if index > 0 {
-		return sl.names[index-1], index, true
+func (*SegmentList) filesToNames(files []fs.DirEntry) []SegmentName {
+	names := make([]SegmentName, 0, len(files))
+	for _, f := range files {
+		if !f.IsDir() {
+			names = append(names, ParseSegmentName(f.Name()))
+		}
 	}
+	// TODO: Do we need to sort?
+	slices.SortFunc(names, func(a, b SegmentName) int {
+		return int(a.ID() - b.ID())
+	})
+	return names
+}
 
-	return SegmentName{}, 0, false
+func (sl *SegmentList) getFiles() ([]fs.DirEntry, error) {
+	return sl.fsys.ReadDir(sl.dir)
 }
 
 func (sl *SegmentList) getIndexForLSN(lsn record.LogSequenceNumber) int {
@@ -198,4 +162,40 @@ func (sl *SegmentList) getIndexForLSN(lsn record.LogSequenceNumber) int {
 	}
 
 	return 0
+}
+
+func (sl *SegmentList) getLatestSegment() (SegmentName, bool) {
+	if len(sl.names) > 0 {
+		return sl.names[len(sl.names)-1], true
+	}
+
+	return SegmentName{}, false
+}
+
+func (sl *SegmentList) getNextSegment(lsn record.LogSequenceNumber) SegmentName {
+	if len(sl.names) > 0 {
+		return sl.names[len(sl.names)-1].Next(lsn)
+	}
+
+	return SegmentName{}
+}
+
+func (sl *SegmentList) getSegmentBeforeLSN(lsn record.LogSequenceNumber) (SegmentName, int, bool) {
+	index := sl.getIndexForLSN(lsn)
+	if index > 0 {
+		return sl.names[index-1], index, true
+	}
+
+	return SegmentName{}, 0, false
+}
+
+func (sl *SegmentList) getSegmentForLSN(lsn record.LogSequenceNumber) (SegmentName, bool) {
+	for i := len(sl.names) - 1; i >= 0; i-- {
+		n := sl.names[i]
+		if lsn >= n.lsn {
+			return n, true
+		}
+	}
+
+	return SegmentName{}, false
 }
