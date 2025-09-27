15ca293445aa46e8442cfe79b3a3edf2d18da6a8
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-09-27 10:05:07 -0700

    Created NewPageIDFromSize.

--- services/liaradb/log/page/page_id.go
+++ services/liaradb/log/page/page_id.go
@@ -9,6 +9,17 @@ type PageID uint64
 
 const pageIDSize = 8
 
+func NewPageIDFromSize(size int64, pageSize int64) PageID {
+	if pageSize == 0 {
+		return 0
+	}
+	pid := size / pageSize
+	if size%pageSize != 0 {
+		pid++
+	}
+	return PageID(pid)
+}
+
 func (pid PageID) Write(w io.Writer) error {
 	return binary.Write(w, binary.BigEndian, pid)
 }
--- services/liaradb/log/page/page_id_test.go
+++ services/liaradb/log/page/page_id_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cardboardrobots/assert"
 )
 
-func TestPageIDTest(t *testing.T) {
+func TestPageID(t *testing.T) {
 	t.Parallel()
 
 	r, w := assert.NewReaderWriter()
@@ -28,5 +28,45 @@ func TestPageIDTest(t *testing.T) {
 
 	if pid != pid2 {
 		t.Errorf("incorrect value: %v, expected: %v", pid2, pid)
+	}
+}
+
+func TestPageID_NewPageIDFromSize(t *testing.T) {
+	t.Parallel()
+
+	for message, c := range map[string]struct {
+		skip     bool
+		size     int64
+		pageSize int64
+		id       PageID
+	}{
+		"should handle zero pageSize": {
+			size:     10,
+			pageSize: 0,
+			id:       0},
+		"should handle zero size": {
+			size:     0,
+			pageSize: 10,
+			id:       0},
+		"should handle multiple size": {
+			size:     20,
+			pageSize: 10,
+			id:       2},
+		"should handle remainder size": {
+			size:     22,
+			pageSize: 10,
+			id:       3},
+	} {
+		t.Run(message, func(t *testing.T) {
+			t.Parallel()
+			if c.skip {
+				t.Skip()
+			}
+
+			id := NewPageIDFromSize(c.size, c.pageSize)
+			if id != c.id {
+				t.Errorf("%v: incorrect id: %v, expected: %v", message, id, c.id)
+			}
+		})
 	}
 }
