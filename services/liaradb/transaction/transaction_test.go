35c165cbaa9f1916c16df0852e9365c14f0b3f3f
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-10-04 15:17:03 -0700

    Fixed Transaction test formatting.

--- services/liaradb/transaction/transaction_test.go
+++ services/liaradb/transaction/transaction_test.go
@@ -14,83 +14,87 @@ import (
 
 func TestTransaction_Insert(t *testing.T) {
 	t.Parallel()
-	synctest.Test(t, func(t *testing.T) {
-		l := createLog(t)
-		defer l.Close()
-		ctx := t.Context()
+	synctest.Test(t, testTransaction_Insert)
+}
 
-		var tid record.TransactionID = 1
-		tx := NewTransaction(tid, l)
+func testTransaction_Insert(t *testing.T) {
+	l := createLog(t)
+	defer l.Close()
+	ctx := t.Context()
 
-		if err := tx.Insert(ctx, time.UnixMicro(1234567890), nil); err != nil {
+	var tid record.TransactionID = 1
+	tx := NewTransaction(tid, l)
+
+	if err := tx.Insert(ctx, time.UnixMicro(1234567890), nil); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := l.Flush(ctx, tx.LogSequenceNumber()); err != nil {
+		t.Fatal(err)
+	}
+
+	c := 0
+	for rc, err := range l.Iterate(0) {
+		if err != nil {
 			t.Fatal(err)
 		}
 
-		if err := l.Flush(ctx, tx.LogSequenceNumber()); err != nil {
-			t.Fatal(err)
+		if lsn := tx.LogSequenceNumber(); lsn != rc.LogSequenceNumber() {
+			t.Errorf("lsn does not match: %v, expected: %v", lsn, rc.LogSequenceNumber())
 		}
 
-		c := 0
-		for rc, err := range l.Iterate(0) {
-			if err != nil {
-				t.Fatal(err)
-			}
+		c++
+	}
 
-			if lsn := tx.LogSequenceNumber(); lsn != rc.LogSequenceNumber() {
-				t.Errorf("lsn does not match: %v, expected: %v", lsn, rc.LogSequenceNumber())
-			}
-
-			c++
-		}
-
-		if c != 1 {
-			t.Errorf("incorrect record count: %v, expected: %v", c, 1)
-		}
-	})
+	if c != 1 {
+		t.Errorf("incorrect record count: %v, expected: %v", c, 1)
+	}
 }
 
 func TestTransaction_Commit(t *testing.T) {
 	t.Parallel()
-	synctest.Test(t, func(t *testing.T) {
-		l := createLog(t)
-		defer l.Close()
-		ctx := t.Context()
+	synctest.Test(t, testTransaction_Commit)
+}
 
-		var tid record.TransactionID = 1
-		tx := NewTransaction(tid, l)
+func testTransaction_Commit(t *testing.T) {
+	l := createLog(t)
+	defer l.Close()
+	ctx := t.Context()
 
-		if err := tx.Insert(ctx, time.UnixMicro(1234567890), nil); err != nil {
+	var tid record.TransactionID = 1
+	tx := NewTransaction(tid, l)
+
+	if err := tx.Insert(ctx, time.UnixMicro(1234567890), nil); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := tx.Commit(ctx, time.UnixMicro(1234567890)); err != nil {
+		t.Fatal(err)
+	}
+
+	lsns := []record.LogSequenceNumber{1, 2}
+	actions := []record.Action{record.ActionInsert, record.ActionCommit}
+
+	c := 0
+	for rc, err := range l.Iterate(0) {
+		if err != nil {
 			t.Fatal(err)
 		}
 
-		if err := tx.Commit(ctx, time.UnixMicro(1234567890)); err != nil {
-			t.Fatal(err)
+		if lsn := rc.LogSequenceNumber(); lsn != lsns[c] {
+			t.Errorf("lsn does not match: %v, expected: %v", lsn, lsns[c])
 		}
 
-		lsns := []record.LogSequenceNumber{1, 2}
-		actions := []record.Action{record.ActionInsert, record.ActionCommit}
-
-		c := 0
-		for rc, err := range l.Iterate(0) {
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if lsn := rc.LogSequenceNumber(); lsn != lsns[c] {
-				t.Errorf("lsn does not match: %v, expected: %v", lsn, lsns[c])
-			}
-
-			if a := rc.Action(); a != actions[c] {
-				t.Errorf("action does not match: %v, expected: %v", a, actions[c])
-			}
-
-			c++
+		if a := rc.Action(); a != actions[c] {
+			t.Errorf("action does not match: %v, expected: %v", a, actions[c])
 		}
 
-		if c != 2 {
-			t.Errorf("incorrect record count: %v, expected: %v", c, 2)
-		}
-	})
+		c++
+	}
+
+	if c != 2 {
+		t.Errorf("incorrect record count: %v, expected: %v", c, 2)
+	}
 }
 
 func createLog(t *testing.T) *log.Log {
