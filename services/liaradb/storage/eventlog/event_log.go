1777f8af6ecf9f6245b6f67b40d96cb527ab360b
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-10-07 15:22:40 -0700

    Created EventLog.Append logic.

--- services/liaradb/storage/eventlog/event_log.go
+++ services/liaradb/storage/eventlog/event_log.go
@@ -1,6 +1,8 @@
 package eventlog
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 
 	"github.com/liaradb/liaradb/storage"
@@ -8,21 +10,33 @@ import (
 
 type EventLog struct {
 	storage *storage.Storage
+	buffer  *bytes.Buffer
+	reader  *bufio.Reader
 }
 
 func New(
 	storage *storage.Storage,
 ) *EventLog {
+	buffer := bytes.NewBuffer(nil)
+	reader := bufio.NewReader(buffer)
 	return &EventLog{
 		storage: storage,
+		buffer:  buffer,
+		reader:  reader,
 	}
 }
 
 func (l *EventLog) Append(ctx context.Context, fileName string, e *Event) error {
-	_, err := l.storage.RequestLatest(ctx, fileName)
+	var data []byte
+	if _, err := l.buffer.Write(data); err != nil {
+		return err
+	}
+
+	_, err := l.storage.Append(ctx, fileName, l.reader)
 	if err != nil {
 		return err
 	}
 
+	l.buffer.Reset()
 	return nil
 }
