add14fca943d5e6ab947f5d79bc932b3f9ffbf6e
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-10-21 22:04:22 -0700

    Created AggregateName encoding.

--- services/liaradb/domain/value/aggregate_name.go
+++ services/liaradb/domain/value/aggregate_name.go
@@ -1,5 +1,14 @@
 package value
 
+import (
+	"io"
+
+	"github.com/liaradb/liaradb/raw"
+)
+
 type AggregateName string
 
-func (an AggregateName) String() string { return string(an) }
+func (an AggregateName) String() string         { return string(an) }
+func (i AggregateName) Size() int               { return raw.StringSize(i) }
+func (i AggregateName) Write(w io.Writer) error { return raw.WriteString(w, i) }
+func (i *AggregateName) Read(r io.Reader) error { return raw.ReadString(r, i) }
