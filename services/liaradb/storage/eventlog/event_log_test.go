a289b6ed342b06f2f46193a0a91ad47d10f65300
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-10-23 08:21:57 -0700

    Created EventLog.Append test.

--- services/liaradb/storage/eventlog/event_log_test.go
+++ services/liaradb/storage/eventlog/event_log_test.go
@@ -1,13 +1,85 @@
 package eventlog
 
 import (
+	"bytes"
 	"path"
 	"slices"
 	"testing"
 	"testing/synctest"
 
+	"github.com/liaradb/liaradb/domain/entity"
+	"github.com/liaradb/liaradb/domain/value"
 	"github.com/liaradb/liaradb/raw"
 )
+
+func TestEventLog_Append(t *testing.T) {
+	t.Parallel()
+	t.Skip()
+	synctest.Test(t, testEventLog_Append)
+}
+
+func testEventLog_Append(t *testing.T) {
+	ctx := t.Context()
+	el := New(createStorage(t, 2, 1024))
+	fn := path.Join(t.TempDir(), "testfile")
+
+	records := []*entity.Event{{
+		GlobalVersion: value.NewGlobalVersion(0),
+		ID:            value.NewEventID(),
+	}, {
+		GlobalVersion: value.NewGlobalVersion(1),
+		ID:            value.NewEventID(),
+	}, {
+		GlobalVersion: value.NewGlobalVersion(2),
+		ID:            value.NewEventID(),
+	}, {
+		GlobalVersion: value.NewGlobalVersion(3),
+		ID:            value.NewEventID(),
+	}, {
+		GlobalVersion: value.NewGlobalVersion(4),
+		ID:            value.NewEventID(),
+	}}
+
+	for _, r := range records {
+		if err := el.Append(ctx, fn, r); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// pageCount := 0
+	result := make([]*entity.Event, 0)
+
+	buf := bytes.NewBuffer(nil)
+
+	for b, err := range el.Iterate(ctx, fn) {
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// pageCount++
+
+		for i, err := range b.Items() {
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			buf = bytes.NewBuffer(i)
+
+			var e entity.Event
+			e.Read(buf)
+
+			result = append(result, &e)
+		}
+	}
+
+	// if pageCount != 3 {
+	// 	t.Errorf("incorrect page count: %v, expected: %v", pageCount, 3)
+	// }
+
+	if !slices.Equal(result, records) {
+		t.Errorf("incorrect result: %v, expected: %v", result, records)
+	}
+}
 
 func TestEventLog_AppendEvent(t *testing.T) {
 	t.Parallel()
