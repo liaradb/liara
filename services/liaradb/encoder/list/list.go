5a4d3e1aeab346402396f0cb96229ffc0a0151bb
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-11-12 18:03:15 -0700

    Cleaned list.List.

--- services/liaradb/encoder/list/list.go
+++ services/liaradb/encoder/list/list.go
@@ -17,15 +17,27 @@ type List struct {
 }
 
 func New(data []byte) *List {
-	var size int32
-	if len(data) >= 4 {
-		size = int32(binary.BigEndian.Uint32(data))
-	}
-
 	return &List{
-		size: size,
+		size: getInt32(data, 0),
 		data: data,
 	}
+}
+
+func getInt32(data []byte, offset int32) int32 {
+	if len(data) < int(offset+4) {
+		return 0
+	}
+
+	return int32(binary.BigEndian.Uint32(data[4:]))
+}
+
+func setInt32(data []byte, offset int32, value int32) bool {
+	if len(data) < int(offset+4) {
+		return false
+	}
+
+	binary.BigEndian.PutUint32(data[offset:], uint32(value))
+	return true
 }
 
 func (l *List) Length() int {
@@ -37,12 +49,9 @@ func (l *List) Size() int32 {
 }
 
 func (l *List) setSize(size int32) {
-	if l.Length() < 4 {
-		return
+	if setInt32(l.data, 0, size) {
+		l.size = size
 	}
-
-	l.size = size
-	binary.BigEndian.PutUint32(l.data, uint32(size))
 }
 
 func (l *List) value(offset int32) (int32, bool) {
@@ -55,17 +64,10 @@ func (l *List) value(offset int32) (int32, bool) {
 
 func (l *List) Item(index int32) (int32, bool) {
 	return l.value(l.offset(index))
-
 }
 
 func (l *List) setValue(offset int32, value int32) bool {
-	if len(l.data) < int(offset)+4 {
-		return false
-	}
-
-	binary.BigEndian.PutUint32(l.data[offset:], uint32(value))
-
-	return true
+	return setInt32(l.data, offset, value)
 }
 
 func (l *List) Append(value int32) (int, error) {
