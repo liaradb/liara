6b786b68e547e504e91acc408430bf3aace9c464
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-11-28 13:57:07 -0700

    Created Int16List.

---
+++ services/liaradb/encoder/int16list/int16_list.go
@@ -0,0 +1,46 @@
+package int16list
+
+import "encoding/binary"
+
+const (
+	itemSize = 4
+)
+
+type Int16List struct {
+	data []byte
+}
+
+func New(data []byte) Int16List {
+	return Int16List{
+		data: data,
+	}
+}
+
+func (l Int16List) Length() int {
+	return len(l.data)
+}
+
+func (l Int16List) Size() int16 {
+	return int16(len(l.data) / itemSize)
+}
+
+func (l Int16List) Get(index int16) (int16, bool) {
+	if index >= l.Size() {
+		return 0, false
+	}
+
+	return int16(binary.BigEndian.Uint16(l.data[l.offset(index):])), true
+}
+
+func (l Int16List) Set(index int16, value int16) bool {
+	if index >= l.Size() {
+		return false
+	}
+
+	binary.BigEndian.PutUint16(l.data[l.offset(index):], uint16(value))
+	return true
+}
+
+func (l Int16List) offset(index int16) int16 {
+	return index * itemSize
+}
