5558e64161b3cde01fa694fcdebf98646769a5b1
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-12-05 12:42:00 -0700

    Created KeyNode test.

--- services/liaradb/collection/btree/key_node.go
+++ services/liaradb/collection/btree/key_node.go
@@ -10,11 +10,18 @@ type KeyNode struct {
 	page page.BTreePage
 }
 
+func newKeyNode(page page.BTreePage) *KeyNode {
+	return &KeyNode{
+		page: page,
+	}
+}
+
 func (kn *KeyNode) Init(p BlockPosition) {
 	kn.page.SetLowID(p.Value())
 }
 
-func (kn *KeyNode) Insert(ke KeyEntry) (int16, bool) {
+// TODO: Test this
+func (kn *KeyNode) Append(ke KeyEntry) (int16, bool) {
 	i, b, ok := kn.page.Append(int16(ke.Size()))
 	if !ok {
 		return 0, false
@@ -28,7 +35,22 @@ func (kn *KeyNode) Insert(ke KeyEntry) (int16, bool) {
 	return i, true
 }
 
-// TODO: Test this
+func (kn *KeyNode) Insert(ke KeyEntry) (int16, bool) {
+	i, _ := kn.SearchIndex(ke.key)
+
+	i, b, ok := kn.page.Insert(int16(ke.Size()), i)
+	if !ok {
+		return 0, false
+	}
+
+	// TODO: Change to bool instead of error
+	if err := ke.Write(b); err != nil {
+		return 0, false
+	}
+
+	return i, true
+}
+
 // TODO: Change to bool instead of error
 func (kn *KeyNode) Children() iter.Seq2[KeyEntry, error] {
 	return func(yield func(KeyEntry, error) bool) {
@@ -65,7 +87,7 @@ func (kn *KeyNode) Search(k Key) (BlockPosition, error) {
 }
 
 func (kn *KeyNode) SearchIndex(k Key) (int16, error) {
-	var i int16 = -1
+	var i int16 = 0
 	for ke, err := range kn.Children() {
 		if err != nil {
 			return 0, err
---
+++ services/liaradb/collection/btree/key_node_test.go
@@ -0,0 +1,45 @@
+package btree
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/liaradb/liaradb/collection/btree/page"
+)
+
+func TestKeyNode(t *testing.T) {
+	bp := page.New(make([]byte, 256))
+	kn := newKeyNode(bp)
+
+	data := []KeyEntry{
+		{"a", 1},
+		{"b", 2},
+		{"c", 3},
+	}
+
+	if _, ok := kn.Insert(data[0]); !ok {
+		t.Error("should insert")
+	}
+
+	if _, ok := kn.Insert(data[2]); !ok {
+		t.Error("should insert")
+	}
+
+	if _, ok := kn.Insert(data[1]); !ok {
+		t.Error("should insert")
+	}
+
+	result := make([]KeyEntry, 0, len(data))
+
+	for c, err := range kn.Children() {
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		result = append(result, c)
+	}
+
+	if !slices.Equal(result, data) {
+		t.Errorf("incorrect result: %v, expected: %v", result, data)
+	}
+}
