57d539df536e8239b4f46b3a9504496209c94f47
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-12-22 13:26:20 -0700

    Fixed RecordID.

--- services/liaradb/storage/link/record_id.go
+++ services/liaradb/storage/link/record_id.go
@@ -7,9 +7,19 @@ type RecordID struct {
 	position RecordPosition
 }
 
+func NewRecordID(
+	blockID BlockID,
+	position RecordPosition,
+) RecordID {
+	return RecordID{
+		blockID:  blockID,
+		position: position,
+	}
+}
+
 func (i RecordID) BlockID() BlockID         { return i.blockID }
 func (i RecordID) Position() RecordPosition { return i.position }
 
 func (i RecordID) Offset(bufferSize int64) page.Offset {
-	return i.blockID.Offset(bufferSize) + page.Offset(i.position)
+	return i.blockID.Offset(bufferSize) * page.Offset(i.position)
 }
