50d91a65c3b09db09502b391a9ba67c20b5b5057
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-12-23 10:05:48 -0700

    Created testutil.RunWait.

--- services/liaradb/util/testutil/run.go
+++ services/liaradb/util/testutil/run.go
@@ -12,3 +12,14 @@ func Run(t *testing.T, m string, f func(*testing.T)) {
 		synctest.Test(t, f)
 	})
 }
+
+func RunWait(t *testing.T, m string, f func(*testing.T)) {
+	t.Helper()
+	t.Run(m, func(t *testing.T) {
+		t.Parallel()
+		synctest.Test(t, func(t *testing.T) {
+			f(t)
+			synctest.Wait()
+		})
+	})
+}
