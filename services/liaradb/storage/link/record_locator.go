c89f6d893ee5fc2f1b9969a6d9936081ac55d6e9
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-12-22 18:47:29 -0700

    Created scan.Byte.  Fixed RecordLocator Read and Write.

--- services/liaradb/storage/link/record_locator.go
+++ services/liaradb/storage/link/record_locator.go
@@ -1,7 +1,7 @@
 package link
 
 import (
-	"github.com/liaradb/liaradb/encoder/wrap"
+	"github.com/liaradb/liaradb/encoder/scan"
 )
 
 const RecordIDSize = 8 + 1
@@ -23,24 +23,19 @@ func (i RecordLocator) Block() FilePosition { return i.block }
 func (i RecordLocator) Position() int8      { return i.position.Value() }
 func (i RecordLocator) Size() int           { return RecordIDSize }
 
-// TODO: Use simpler wrap
 func (le RecordLocator) Write(data []byte) []byte {
-	block, data0 := wrap.NewInt64(data)
-	position, data1 := wrap.NewByte(data0)
-
-	block.Set(le.block.Value())
-	position.Set(le.position.Value())
+	data0 := scan.SetInt64(data, le.block.Value())
+	data1 := scan.SetInt8(data0, le.position.Value())
 
 	return data1
 }
 
-// TODO: Use simpler wrap
 func (le *RecordLocator) Read(data []byte) []byte {
-	block, data0 := wrap.NewInt64(data)
-	position, data1 := wrap.NewByte(data0)
+	block, data0 := scan.Int64(data)
+	position, data1 := scan.Int8(data0)
 
-	le.block = FilePosition(block.Get())
-	le.position = RecordPosition(position.Get())
+	le.block = FilePosition(block)
+	le.position = RecordPosition(position)
 
 	return data1
 }
