dc40509825541f53a0933c34d7cc60a5fe9ba77c
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-12-23 10:28:38 -0700

    Created manager.Manager.

---
+++ services/liaradb/collection/manager/manager.go
@@ -0,0 +1,40 @@
+package table
+
+import (
+	"context"
+
+	"github.com/liaradb/liaradb/collection/btree/value"
+	"github.com/liaradb/liaradb/collection/keyvalue"
+	"github.com/liaradb/liaradb/collection/tablename"
+	"github.com/liaradb/liaradb/encoder/raw"
+	"github.com/liaradb/liaradb/storage"
+)
+
+type Manager struct {
+	s  *storage.Storage
+	kv *keyvalue.KeyValue
+}
+
+func New(s *storage.Storage) *Manager {
+	return &Manager{
+		s:  s,
+		kv: keyvalue.New(s),
+	}
+}
+
+func (m *Manager) Insert(ctx context.Context, k value.Key, i int64) error {
+	b := raw.NewBuffer(8)
+	raw.WriteInt64(b, i)
+	return m.kv.Set(ctx, tablename.New("tables"), k, b.Bytes())
+}
+
+func (m *Manager) List(ctx context.Context, k value.Key) (int64, error) {
+	d, err := m.kv.Get(ctx, tablename.New("tables"), k)
+	if err != nil {
+		return 0, err
+	}
+
+	b := raw.NewBufferFromSlice(d)
+	var i int64
+	return i, raw.ReadInt64(b, &i)
+}
---
+++ services/liaradb/collection/manager/manager_test.go
@@ -0,0 +1,32 @@
+package table
+
+import (
+	"testing"
+	"testing/synctest"
+
+	"github.com/liaradb/liaradb/storage/storagetesting"
+)
+
+func TestManager(t *testing.T) {
+	t.Parallel()
+	synctest.Test(t, testManager)
+}
+
+func testManager(t *testing.T) {
+	s := storagetesting.CreateStorage(t, 2, 256)
+	m := New(s)
+
+	var want int64 = 2
+	if err := m.Insert(t.Context(), "a", want); err != nil {
+		t.Fatal(err)
+	}
+
+	i, err := m.List(t.Context(), "a")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if i != want {
+		t.Errorf("incorrect result: %v, expected: %v", i, want)
+	}
+}
