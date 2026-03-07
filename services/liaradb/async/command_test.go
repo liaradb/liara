fb415a942ac6895dbff0efbd1f360b72f00d89a9
Sean Johnson <sean.johnson.arizona@gmail.com>
2026-03-06 21:55:18 -0700

    Created async tests.

--- services/liaradb/async/command_test.go
+++ services/liaradb/async/command_test.go
@@ -1,6 +1,7 @@
 package async
 
 import (
+	"context"
 	"errors"
 	"testing"
 	"testing/synctest"
@@ -17,6 +18,10 @@ func testCommand(t *testing.T) {
 
 	go func() {
 		if r, ok := h.Listen(t.Context()); ok {
+			if v := r.Value(); v != "a" {
+				t.Errorf("incorrect value: %v, expected: %v", v, "a")
+			}
+
 			r.Reply(errValue)
 		}
 	}()
@@ -24,5 +29,65 @@ func testCommand(t *testing.T) {
 	err := h.Send(t.Context(), "a")
 	if !errors.Is(err, errValue) {
 		t.Errorf("incorrect error: %v, expected: %v", err, errValue)
+	}
+}
+
+func TestCommand_Listen__Canceled(t *testing.T) {
+	t.Parallel()
+	synctest.Test(t, testCommand_Listen__Canceled)
+}
+
+func testCommand_Listen__Canceled(t *testing.T) {
+	h := make(CommandHandler[string])
+
+	ctx, cancel := context.WithCancel(t.Context())
+	cancel()
+
+	if _, ok := h.Listen(ctx); ok {
+		t.Error("should return false")
+	}
+}
+
+func TestCommand_Send__Canceled(t *testing.T) {
+	t.Parallel()
+	synctest.Test(t, testCommand_Send__Canceled)
+}
+
+func testCommand_Send__Canceled(t *testing.T) {
+	h := make(CommandHandler[string])
+
+	ctx, cancel := context.WithCancel(t.Context())
+	cancel()
+
+	if err := h.Send(ctx, ""); err == nil {
+		t.Error("should be canceled")
+	}
+}
+
+func TestCommand__Wait__Canceled(t *testing.T) {
+	t.Parallel()
+	synctest.Test(t, testCommand__Wait__Canceled)
+}
+
+func testCommand__Wait__Canceled(t *testing.T) {
+	h := make(CommandHandler[string])
+
+	ctx, cancel := context.WithCancel(t.Context())
+
+	go func() {
+		_, ok := h.Listen(ctx)
+		if !ok {
+			t.Error("should return true")
+		}
+	}()
+
+	go func() {
+		synctest.Wait()
+		cancel()
+	}()
+
+	err := h.Send(ctx, "")
+	if err == nil {
+		t.Error("should be canceled")
 	}
 }
--- services/liaradb/async/request_test.go
+++ services/liaradb/async/request_test.go
@@ -1,6 +1,7 @@
 package async
 
 import (
+	"context"
 	"errors"
 	"testing"
 	"testing/synctest"
@@ -17,6 +18,14 @@ func testRequest(t *testing.T) {
 
 	go func() {
 		if r, ok := h.Listen(t.Context()); ok {
+			if v := r.Value(); v != "a" {
+				t.Errorf("incorrect value: %v, expected: %v", v, "a")
+			}
+
+			if c := r.Context(); c != t.Context() {
+				t.Errorf("incorrect context: %v, expected: %v", c, t.Context())
+			}
+
 			r.Reply(2, errValue)
 		}
 	}()
@@ -27,5 +36,65 @@ func testRequest(t *testing.T) {
 	}
 	if !errors.Is(err, errValue) {
 		t.Errorf("incorrect error: %v, expected: %v", err, errValue)
+	}
+}
+
+func TestRequest_Listen__Canceled(t *testing.T) {
+	t.Parallel()
+	synctest.Test(t, testRequest_Listen__Canceled)
+}
+
+func testRequest_Listen__Canceled(t *testing.T) {
+	h := make(Handler[string, string])
+
+	ctx, cancel := context.WithCancel(t.Context())
+	cancel()
+
+	if _, ok := h.Listen(ctx); ok {
+		t.Error("should return false")
+	}
+}
+
+func TestRequest_Send__Canceled(t *testing.T) {
+	t.Parallel()
+	synctest.Test(t, testRequest_Send__Canceled)
+}
+
+func testRequest_Send__Canceled(t *testing.T) {
+	h := make(Handler[string, string])
+
+	ctx, cancel := context.WithCancel(t.Context())
+	cancel()
+
+	if _, err := h.Send(ctx, ""); err == nil {
+		t.Error("should be canceled")
+	}
+}
+
+func TestRequest__Wait__Canceled(t *testing.T) {
+	t.Parallel()
+	synctest.Test(t, testRequest__Wait__Canceled)
+}
+
+func testRequest__Wait__Canceled(t *testing.T) {
+	h := make(Handler[string, string])
+
+	ctx, cancel := context.WithCancel(t.Context())
+
+	go func() {
+		_, ok := h.Listen(ctx)
+		if !ok {
+			t.Error("should return true")
+		}
+	}()
+
+	go func() {
+		synctest.Wait()
+		cancel()
+	}()
+
+	_, err := h.Send(ctx, "")
+	if err == nil {
+		t.Error("should be canceled")
 	}
 }
