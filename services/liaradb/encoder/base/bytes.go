f44dc60cd004ea3f5c7b22ca92588e994abd805e
Sean Johnson <sean.johnson.arizona@gmail.com>
2026-03-08 21:35:34 -0700

    Created Bytes test.

--- services/liaradb/encoder/base/bytes.go
+++ services/liaradb/encoder/base/bytes.go
@@ -22,3 +22,16 @@ func (b *Bytes) Length() int             { return len(b.data) }
 func (b *Bytes) Size() int               { return len(b.data) + raw.HeaderSize }
 func (b *Bytes) Write(w io.Writer) error { return raw.Write(w, b.data) }
 func (b *Bytes) Read(r io.Reader) error  { return raw.Read(r, &b.data) }
+
+func (b Bytes) WriteData(data []byte, colSize int) []byte {
+	data[0] = byte(len(b.data))
+	copy(data[1:colSize], []byte(b.data))
+	clear(data[1+len(b.data) : colSize])
+	return data[colSize:]
+}
+
+func (b *Bytes) ReadData(data []byte, colSize int) []byte {
+	l := data[0]
+	b.data = data[1 : 1+l]
+	return data[colSize:]
+}
---
+++ services/liaradb/encoder/base/bytes_test.go
@@ -0,0 +1,40 @@
+package base
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestBytes(t *testing.T) {
+	size := 32
+	n := NewBytes([]byte("name"))
+	data := make([]byte, size)
+	_ = n.WriteData(data, size)
+
+	var r Bytes
+	data0 := r.ReadData(data, size)
+
+	if l := len(data0); l != 0 {
+		t.Errorf("incorrect length: %v, expected: %v", l, 0)
+	}
+
+	if !r.Compare(n) {
+		t.Error("should be equal")
+	}
+
+	if r.String() != n.String() {
+		t.Errorf("incorrect result: %v, expected: %v", r.String(), n.String())
+	}
+
+	if b := r.Value(); !slices.Equal(b, []byte("name")) {
+		t.Errorf("incorrect bytes: %v, expected: %v", b, []byte("name"))
+	}
+
+	if l := r.Length(); l != 4 {
+		t.Errorf("incorrect length: %v, expected: %v", l, 4)
+	}
+
+	if s := r.Size(); s != 4+4 {
+		t.Errorf("incorrect size: %v, expected: %v", s, 4+4)
+	}
+}
