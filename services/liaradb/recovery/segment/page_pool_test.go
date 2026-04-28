7c622b691d2530a967e8cde40f20d38b2b65ce60
Sean Johnson <sean.johnson.arizona@gmail.com>
2026-04-28 01:09:13 -0700

    Created PagePool.

---
+++ services/liaradb/recovery/segment/page_pool_test.go
@@ -0,0 +1,26 @@
+package segment
+
+import (
+	"testing"
+
+	"github.com/liaradb/liaradb/recovery/action"
+	"github.com/liaradb/liaradb/recovery/record"
+)
+
+func TestPagePool(t *testing.T) {
+	pp := NewPagePool(1024)
+
+	p0 := pp.Get(action.PageID(1), action.TimeLineID(2), record.NewLength(3))
+	pp.Return(p0)
+
+	if i := p0.ID(); i != action.PageID(1) {
+		t.Errorf("incorrect id: %v, expected: %v", i, action.PageID(1))
+	}
+
+	p1 := pp.Get(action.PageID(10), action.TimeLineID(20), record.NewLength(30))
+	pp.Return(p1)
+
+	if i := p1.ID(); i != action.PageID(10) {
+		t.Errorf("incorrect id: %v, expected: %v", i, action.PageID(10))
+	}
+}
