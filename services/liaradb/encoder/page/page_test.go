39a2a9781dc6ecf715b26275339442f2faf50f5b
Sean Johnson <sean.johnson.arizona@gmail.com>
2026-05-25 23:40:23 -0700

    Created Page tests.

--- services/liaradb/encoder/page/page_test.go
+++ services/liaradb/encoder/page/page_test.go
@@ -1,11 +1,57 @@
 package page
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
-func TestPage(t *testing.T) {
+func TestPage_New(t *testing.T) {
 	t.Parallel()
 
 	p := New(32, 4, 4)
+
+	want := make([]byte, 32)
+	if data := p.Data(); !slices.Equal(data, want) {
+		t.Errorf("incorrect data: %v, expected: %v", data, want)
+	}
+}
+
+func TestPage_NewFromSlice(t *testing.T) {
+	t.Parallel()
+
+	want := []byte{1, 2, 3, 4}
+	p := NewFromSlice(want, 4, 4)
+
+	if data := p.Data(); !slices.Equal(data, want) {
+		t.Errorf("incorrect data: %v, expected: %v", data, want)
+	}
+}
+
+func TestPage_Fill(t *testing.T) {
+	t.Parallel()
+
+	p := New(4, 4, 4)
+
+	want := []byte{1, 2, 3, 4}
+	p.Fill(want)
+
+	if data := p.Data(); !slices.Equal(data, want) {
+		t.Errorf("incorrect data: %v, expected: %v", data, want)
+	}
+
+	p.Clear()
+
+	want = make([]byte, 4)
+	if data := p.Data(); !slices.Equal(data, want) {
+		t.Errorf("incorrect data: %v, expected: %v", data, want)
+	}
+}
+
+func TestPage_Next(t *testing.T) {
+	t.Parallel()
+
+	p := New(32, 4, 4)
+
 	header, data := p.Next(8)
 
 	if l := len(header); l != 4 {
