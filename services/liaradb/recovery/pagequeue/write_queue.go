3d6262cb85d0218a6ad5919bdf919759fd736b84
Sean Johnson <sean.johnson.arizona@gmail.com>
2026-06-13 18:19:06 -0700

    Created WriteQueue.

---
+++ services/liaradb/recovery/pagequeue/write_queue.go
@@ -0,0 +1,86 @@
+package pagequeue
+
+import (
+	"context"
+
+	"github.com/liaradb/liaradb/encoder/page"
+	"github.com/liaradb/liaradb/recovery/record"
+)
+
+type queueItem struct {
+	lsn  record.LogSequenceNumber
+	page *page.Page
+	sync bool
+}
+
+type WriteQueue struct {
+	current *page.Page
+	items   chan queueItem
+	ps      PageStorage
+	fl      Flusher
+}
+
+type Flusher interface {
+	OnFlush(record.LogSequenceNumber)
+	OnError(error) bool
+}
+
+func newWriteQueue(
+	size int,
+	ps PageStorage,
+	fl Flusher,
+) *WriteQueue {
+	return &WriteQueue{
+		items: make(chan queueItem, size),
+		ps:    ps,
+		fl:    fl,
+	}
+}
+
+func (wq *WriteQueue) Run(ctx context.Context) {
+	go wq.run(ctx)
+}
+
+func (wq *WriteQueue) run(ctx context.Context) {
+	for {
+		select {
+		case qi := <-wq.items:
+			if !wq.runItem(qi) {
+				return
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (wq *WriteQueue) runItem(qi queueItem) bool {
+	if err := wq.storeItem(qi); err != nil {
+		return wq.fl.OnError(err)
+	}
+
+	wq.notifyFlush(qi)
+	return true
+}
+
+func (wq *WriteQueue) storeItem(qi queueItem) error {
+	if qi.sync {
+		return wq.ps.Sync(qi.page.Data())
+	}
+
+	return wq.ps.Append(qi.lsn, qi.page.Data())
+}
+
+func (wq *WriteQueue) notifyFlush(qi queueItem) {
+	if qi.lsn.Value() != 0 {
+		wq.fl.OnFlush(qi.lsn)
+	}
+}
+
+func (wq *WriteQueue) Append(lsn record.LogSequenceNumber, p *page.Page) {
+	wq.items <- queueItem{lsn, p, false}
+}
+
+func (wq *WriteQueue) Sync(lsn record.LogSequenceNumber, p *page.Page) {
+	wq.items <- queueItem{lsn, p, true}
+}
---
+++ services/liaradb/recovery/pagequeue/write_queue_test.go
@@ -0,0 +1,57 @@
+package pagequeue
+
+import (
+	"sync"
+	"testing"
+	"testing/synctest"
+
+	"github.com/liaradb/liaradb/encoder/page"
+	"github.com/liaradb/liaradb/recovery/record"
+)
+
+func TestWriteQueue(t *testing.T) {
+	t.Parallel()
+	synctest.Test(t, testWriteQueueTestWriteQueue)
+}
+
+func testWriteQueueTestWriteQueue(t *testing.T) {
+	wg := sync.WaitGroup{}
+
+	ps := &testPageStorage{}
+	wq := newWriteQueue(10, ps, &testFlusher{
+		onFlush: func(lsn record.LogSequenceNumber) {
+			if v := lsn.Value(); v != 1 {
+				t.Errorf("incorrect lsn: %v, expected: %v", v, 1)
+			}
+			wg.Done()
+		},
+	})
+	wq.Run(t.Context())
+
+	wg.Add(1)
+	wq.Append(record.NewLogSequenceNumber(0), page.New(128, 16, 8))
+	wq.Append(record.NewLogSequenceNumber(1), page.New(128, 16, 8))
+	wg.Wait()
+
+	if ps.appendCount != 2 {
+		t.Errorf("incorrect append count: %v, expected: %v", ps.appendCount, 2)
+	}
+
+	if ps.syncCount != 0 {
+		t.Errorf("incorrect sync count: %v, expected: %v", ps.syncCount, 0)
+	}
+}
+
+type testFlusher struct {
+	onFlush func(record.LogSequenceNumber)
+}
+
+func (t *testFlusher) OnError(error) bool {
+	return true
+}
+
+func (t *testFlusher) OnFlush(lsn record.LogSequenceNumber) {
+	t.onFlush(lsn)
+}
+
+var _ Flusher = &testFlusher{}
