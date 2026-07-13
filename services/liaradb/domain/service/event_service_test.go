cec00939fa32ff5d03b12b7e6ef889ce71095d8e
Sean Johnson <sean.johnson.arizona@gmail.com>
2026-07-12 22:42:58 -0700

    Created EventService test.

--- services/liaradb/domain/service/event_service_test.go
+++ services/liaradb/domain/service/event_service_test.go
@@ -4,9 +4,17 @@ import (
 	"context"
 	"errors"
 	"testing"
+	"testing/synctest"
 
 	"github.com/google/uuid"
 	"github.com/liaradb/liaradb/domain/value"
+	"github.com/liaradb/liaradb/filecache"
+	"github.com/liaradb/liaradb/locktable"
+	"github.com/liaradb/liaradb/recovery"
+	"github.com/liaradb/liaradb/recovery/action"
+	"github.com/liaradb/liaradb/transaction"
+	"github.com/liaradb/liaradb/util/testing/filetesting"
+	"github.com/liaradb/liaradb/util/testing/storagetesting"
 )
 
 func TestEventService_Append(t *testing.T) {
@@ -28,4 +36,92 @@ func TestEventService_Append(t *testing.T) {
 			t.Error("should return error")
 		}
 	})
+
+	t.Run("should append", func(t *testing.T) {
+		t.Skip()
+		synctest.Test(t, func(t *testing.T) {
+			m, l := createManager(t)
+			l.Run(t.Context())
+			l.StartWriter()
+			m.Run(t.Context())
+			es := NewEventService(m)
+
+			aggregateID := value.NewAggregateID(uuid.NewString())
+			if err := es.Append(
+				t.Context(),
+				value.NewTenantID(),
+				AppendOptions{},
+				value.NewPartitionID(0),
+				AppendEvent{
+					AggregateName: value.NewAggregateName("example"),
+					// ID:            value.NewEventID(),
+					AggregateID: aggregateID,
+					Version:     value.NewVersion(1),
+				},
+			); err != nil {
+				t.Fatal(err)
+			}
+
+			if err := es.Append(
+				t.Context(),
+				value.NewTenantID(),
+				AppendOptions{},
+				value.NewPartitionID(0),
+				AppendEvent{
+					AggregateName: value.NewAggregateName("example"),
+					// ID:            value.NewEventID(),
+					AggregateID: aggregateID,
+					Version:     value.NewVersion(2),
+				},
+			); err != nil {
+				t.Fatal(err)
+			}
+		})
+	})
+}
+
+func createManager(t *testing.T) (*transaction.Manager, *recovery.Log) {
+	t.Helper()
+
+	fsys, dir := createFiles()
+	l := createLog(t, fsys, dir)
+	s := storagetesting.CreateStorageWithFileSystem(t, 2, 1024, fsys, dir)
+	lt := createLockTable(t)
+	m := transaction.NewManager(l, s, lt)
+	m.Run(t.Context())
+
+	return m, l
+}
+
+func createLog(t *testing.T, fsys filecache.FileSystem, dir string) *recovery.Log {
+	t.Helper()
+
+	l := recovery.NewLog(256, 3, 256, 100, fsys, dir)
+	if err := l.Run(t.Context()); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		if err := l.Close(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	if err := l.StartWriter(); err != nil {
+		t.Fatal(err)
+	}
+
+	return l
+}
+
+func createLockTable(t *testing.T) *locktable.LockTable[action.ItemID] {
+	lt := locktable.New[action.ItemID](1)
+	lt.Run(t.Context())
+	t.Cleanup(lt.Close)
+	return lt
+}
+
+func createFiles() (filecache.FileSystem, string) {
+	// return &disk.FileSystem{}, t.TempDir()
+	return filetesting.New(nil), "."
 }
