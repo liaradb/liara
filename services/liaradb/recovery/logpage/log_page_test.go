534e49c3351a080173a8939c39a3e21f8584950f
Sean Johnson <sean.johnson.arizona@gmail.com>
2026-07-18 09:27:24 -0700

    Created LogPage.Shadow test.

--- services/liaradb/recovery/logpage/log_page_test.go
+++ services/liaradb/recovery/logpage/log_page_test.go
@@ -1,6 +1,9 @@
 package logpage
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestLogPage_AddHandler(t *testing.T) {
 	p := New(256, 8)
@@ -68,5 +71,38 @@ func TestLogPage_Reset(t *testing.T) {
 	p.Complete()
 	if c != 0 {
 		t.Errorf("incorrect count: %v, expected: %v", c, 0)
+	}
+}
+
+func TestLogPage_Shadow(t *testing.T) {
+	p := New(256, 8)
+
+	c := 0
+	p.AddHandler(func() {
+		c++
+	})
+
+	data := []byte{1, 2, 3, 4}
+	copy(p.Data(), data)
+
+	if !slices.Equal(p.Data()[:4], data) {
+		t.Errorf("incorrect data: %v, expected: %v", p.Data()[:4], data)
+	}
+
+	s := New(256, 8)
+	s.Shadow(p)
+
+	if !slices.Equal(s.Data()[:4], data) {
+		t.Errorf("incorrect data: %v, expected: %v", s.Data()[:4], data)
+	}
+
+	p.Complete()
+	if c != 0 {
+		t.Errorf("incorrect count: %v, expected: %v", c, 0)
+	}
+
+	s.Complete()
+	if c != 1 {
+		t.Errorf("incorrect count: %v, expected: %v", c, 1)
 	}
 }
