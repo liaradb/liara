de5c8abded2c8572a8c319d51b8d2f19b7157eee
Sean Johnson <sean.johnson.arizona@gmail.com>
2026-07-20 18:26:38 -0700

    Removed Record reference from PageIterator tests.

--- services/liaradb/recovery/pageiterator/page_iterator_test.go
+++ services/liaradb/recovery/pageiterator/page_iterator_test.go
@@ -3,29 +3,18 @@ package pageiterator
 import (
 	"testing"
 	"testing/synctest"
-	"time"
 
-	"github.com/liaradb/liaradb/domain/value"
 	"github.com/liaradb/liaradb/recovery/logpage"
 	"github.com/liaradb/liaradb/recovery/pagepool"
 	"github.com/liaradb/liaradb/recovery/pagequeue"
 	"github.com/liaradb/liaradb/recovery/pagequeue/pagestorage"
 	"github.com/liaradb/liaradb/recovery/segment"
 	"github.com/liaradb/liaradb/recovery/span"
-	"github.com/liaradb/liaradb/transaction/record"
 	"github.com/liaradb/liaradb/util/testing/filetesting"
 )
 
 func TestPageIterator(t *testing.T) {
 	t.Parallel()
-
-	tid := value.NewTenantID()
-	txid := record.NewTransactionID(2)
-	now := record.NewTime(time.UnixMicro(1234567890))
-	action := record.ActionInsert
-	collection := record.CollectionEvent
-	data := []byte("abcdef")
-	reverse := []byte("fghij")
 
 	t.Run("should run", func(t *testing.T) {
 		t.Parallel()
@@ -46,13 +35,13 @@ func TestPageIterator(t *testing.T) {
 			}
 
 			pl := pagepool.New(size, span.FragmentHeaderSize)
-			pq := pagequeue.New[*record.Record](ps, pl, writeQueueSize)
+			pq := pagequeue.New[*testRecord](ps, pl, writeQueueSize)
 			go pq.Run(t.Context())
 
 			numberOfRecords := 100
 
 			for i := range numberOfRecords {
-				rc := record.New(tid, txid, now, action, collection, data, reverse)
+				rc := newTestRecord(100)
 				rc.SetLogSequenceNumber(logpage.NewLogSequenceNumber(uint64(i)))
 				if err := pq.Append(t.Context(), rc); err != nil {
 					t.Fatal(err)
@@ -70,7 +59,7 @@ func TestPageIterator(t *testing.T) {
 			}
 
 			sl = segment.NewList(fsys, dir, size, 1)
-			pi := New(sl, pl, func() *record.Record { return &record.Record{} })
+			pi := New(sl, pl, func() *testRecord { return newTestRecord(100) })
 
 			c := 0
 			for rc, err := range pi.Forward(logpage.NewLogSequenceNumber(0)) {
@@ -111,13 +100,13 @@ func TestPageIterator(t *testing.T) {
 			}
 
 			pl := pagepool.New(size, span.FragmentHeaderSize)
-			pq := pagequeue.New[*record.Record](ps, pl, writeQueueSize)
+			pq := pagequeue.New[*testRecord](ps, pl, writeQueueSize)
 			go pq.Run(t.Context())
 
 			numberOfRecords := 100
 
 			for i := range numberOfRecords {
-				rc := record.New(tid, txid, now, action, collection, data, reverse)
+				rc := newTestRecord(100)
 				rc.SetLogSequenceNumber(logpage.NewLogSequenceNumber(uint64(i)))
 				if err := pq.Append(t.Context(), rc); err != nil {
 					t.Error(err)
@@ -135,7 +124,7 @@ func TestPageIterator(t *testing.T) {
 			}
 
 			sl = segment.NewList(fsys, dir, size, 1)
-			pi := New(sl, pl, func() *record.Record { return &record.Record{} })
+			pi := New(sl, pl, func() *testRecord { return newTestRecord(100) })
 
 			c := 0
 			reverseIndex := numberOfRecords - 1
