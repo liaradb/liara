835b7d6cb5e418ff97f00d0526e81d704e75399a
Sean Johnson <sean.johnson.arizona@gmail.com>
2026-08-22 09:57:37 -0700

    Created FixedCollection.List.

--- services/liaradb/collection/fixedv2/fixed_collection.go
+++ services/liaradb/collection/fixedv2/fixed_collection.go
@@ -3,10 +3,12 @@ package fixedv2
 import (
 	"context"
 	"errors"
+	"iter"
 
 	"github.com/liaradb/liaradb/collection/btree"
 	"github.com/liaradb/liaradb/collection/btree/key"
 	"github.com/liaradb/liaradb/collection/bufferpage"
+	"github.com/liaradb/liaradb/domain/value"
 	"github.com/liaradb/liaradb/encoder/span"
 	"github.com/liaradb/liaradb/storage"
 	"github.com/liaradb/liaradb/storage/link"
@@ -70,6 +72,78 @@ func (fc *FixedCollection) Get(
 	if err != nil {
 		return nil, err
 	}
+
+	bid := fn.BlockID(rl.Block())
+	b, err := fc.s.Request(ctx, bid)
+	if err != nil {
+		return nil, err
+	}
+
+	bs.Append(b)
+
+	p := bufferpage.New(b)
+	var s span.Span
+
+	h, d, ok := p.Slot(rl.Position())
+	if !ok {
+		return nil, errors.New(" could not read slot")
+	}
+	f := s.Append(h, d)
+	for f.Index() != 0 {
+		bid = bid.Next()
+		b, err := fc.s.Request(ctx, bid)
+		if err != nil {
+			return nil, err
+		}
+
+		bs.Append(b)
+
+		p = bufferpage.New(b)
+		h, d, ok := p.Slot(0)
+		if !ok {
+			return nil, errors.New(" could not read slot")
+		}
+
+		f = s.Append(h, d)
+	}
+
+	// Read Span
+	buffer := make([]byte, s.Length())
+	if _, err := s.Read(buffer); err != nil {
+		return nil, err
+	}
+
+	return buffer, nil
+}
+
+func (fc *FixedCollection) List(
+	ctx context.Context,
+	fn link.FileName,
+	fnIdx link.FileName,
+	pid value.PartitionID,
+) iter.Seq2[[]byte, error] {
+	return func(yield func([]byte, error) bool) {
+		for rid, err := range fc.c.All(ctx, fnIdx, 0, 0) {
+			if err != nil {
+				yield(nil, err)
+				return
+			}
+
+			i, err := fc.getItem(ctx, fn, rid)
+			if !yield(i, err) {
+				return
+			}
+		}
+	}
+}
+
+func (fc *FixedCollection) getItem(
+	ctx context.Context,
+	fn link.FileName,
+	rl link.RecordLocator,
+) ([]byte, error) {
+	var bs bufferSlice
+	defer bs.Release()
 
 	bid := fn.BlockID(rl.Block())
 	b, err := fc.s.Request(ctx, bid)
--- services/liaradb/collection/fixedv2/fixed_collection_test.go
+++ services/liaradb/collection/fixedv2/fixed_collection_test.go
@@ -1,17 +1,26 @@
 package fixedv2
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"slices"
+	"strings"
 	"testing"
+	"testing/synctest"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/liaradb/liaradb/collection/btree"
 	"github.com/liaradb/liaradb/collection/btree/key"
+	"github.com/liaradb/liaradb/domain/entity"
+	"github.com/liaradb/liaradb/domain/value"
 	"github.com/liaradb/liaradb/storage/link"
 	"github.com/liaradb/liaradb/transaction/log"
 	"github.com/liaradb/liaradb/util/testing/storagetesting"
 )
 
-func TestFixedCollection(t *testing.T) {
+func TestFixedCollection_InsertAndGet(t *testing.T) {
 	storagetesting.SyncTest(t, 16, 1024, func(t *testing.T, s storagetesting.Storage) {
 		l := log.New(256, 2, 256, 100, s.FSys, "dir")
 		if err := l.Run(t.Context()); err != nil {
@@ -39,4 +48,184 @@ func TestFixedCollection(t *testing.T) {
 			t.Errorf("incorrect result: %v, expected: %v", result, want)
 		}
 	})
+}
+
+func TestFixedCollection(t *testing.T) {
+	t.Skip()
+	storagetesting.SyncTest(t, 6, 110, func(t *testing.T, s storagetesting.Storage) {
+		ctx := t.Context()
+		l := log.New(256, 2, 256, 100, s.FSys, "dir")
+		if err := l.Run(t.Context()); err != nil {
+			t.Fatal(err)
+		}
+
+		fc := New(s.Storage, btree.NewCursor(s.Storage), l)
+		fn := link.NewFileName("testfile")
+		fnIdx := link.NewFileName("testindex")
+		pid := value.NewPartitionID(0)
+
+		data := createData()
+		slices.Reverse(data)
+
+		if err := insertData(ctx, fc, fn, fnIdx, data); err != nil {
+			t.Fatal(err)
+		}
+
+		testGet(ctx, t, fc, fn, fnIdx, data)
+		testList(ctx, t, data, fc, fn, fnIdx, pid)
+
+		synctest.Wait()
+	})
+}
+
+func TestRequestLog__LargeBuffer(t *testing.T) {
+	t.Skip()
+	storagetesting.SyncTest(t, 2, 256, func(t *testing.T, s storagetesting.Storage) {
+		ctx := t.Context()
+		l := log.New(256, 2, 256, 100, s.FSys, "dir")
+		if err := l.Run(t.Context()); err != nil {
+			t.Fatal(err)
+		}
+
+		fc := New(s.Storage, btree.NewCursor(s.Storage), l)
+		fn := link.NewFileName("testfile")
+		fnIdx := link.NewFileName("testindex")
+		pid := value.NewPartitionID(0)
+
+		data := createData()
+
+		if err := insertData(ctx, fc, fn, fnIdx, data); err != nil {
+			t.Fatal(err)
+		}
+
+		testGet(ctx, t, fc, fn, fnIdx, data)
+		testList(ctx, t, data, fc, fn, fnIdx, pid)
+
+		synctest.Wait()
+	})
+}
+
+type item struct {
+	key   string
+	value *entity.RequestLog
+}
+
+func createData() []item {
+	count := 9
+	data := uuid.UUID{}
+	items := make([]item, 0, count)
+	for i := range count {
+		data[15] = byte(i) + 1
+		rid, _ := value.NewRequestIDFromString(data.String())
+		items = append(items, item{fmt.Sprintf("%v", i+1), entity.NewRequestLog(rid, value.NewTime(time.Now().Add(time.Duration(i)*time.Second)))})
+	}
+	return items
+}
+
+func insertData(ctx context.Context, fc *FixedCollection, fn link.FileName, fnIdx link.FileName, data []item) error {
+	for _, i := range data {
+		d := make([]byte, entity.RequestLogSize)
+
+		if _, ok := i.value.Write(d); !ok {
+			return io.EOF
+		}
+
+		k := key.NewKey(i.value.ID().Bytes())
+		if err := fc.Insert(ctx, fn, fnIdx, k, d); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func testGet(
+	ctx context.Context,
+	t *testing.T,
+	fc *FixedCollection,
+	fn link.FileName,
+	fnIdx link.FileName,
+	data []item,
+) {
+	for _, i := range data {
+		k := key.NewKey(i.value.ID().Bytes())
+		value, err := fc.Get(ctx, fn, fnIdx, k)
+		if err != nil {
+			t.Fatal(i.key, err)
+		}
+
+		rl := entity.RequestLog{}
+		if _, ok := rl.Read(value); !ok {
+			t.Fatal("should read")
+		}
+
+		if rl != *i.value {
+			t.Errorf("incorrect result: %v, expected: %v", rl, *i.value)
+		}
+	}
+}
+
+func testList(
+	ctx context.Context,
+	t *testing.T,
+	data []item,
+	fc *FixedCollection,
+	fn link.FileName,
+	fnIdx link.FileName,
+	pid value.PartitionID,
+) {
+	result, err := getListValues(ctx, data, fc, fn, fnIdx, pid)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := createSortedValues(data)
+	if !slices.Equal(result, want) {
+		t.Errorf("incorrect result: %v, expected: %v", result, want)
+	}
+}
+
+func getListValues(
+	ctx context.Context,
+	data []item,
+	fc *FixedCollection,
+	fn link.FileName,
+	fnIdx link.FileName,
+	pid value.PartitionID,
+) ([]entity.RequestLog, error) {
+	result := make([]entity.RequestLog, 0, len(data))
+	i := 0
+	for value, err := range fc.List(ctx, fn, fnIdx, pid) {
+		if err != nil {
+			return nil, err
+		}
+
+		rl := entity.RequestLog{}
+		if _, ok := rl.Read(value); !ok {
+			return nil, io.EOF
+		}
+
+		result = append(result, rl)
+		i++
+	}
+	return result, nil
+}
+
+func createSortedValues(data []item) []entity.RequestLog {
+	type tuple struct {
+		key   key.Key
+		value *entity.RequestLog
+	}
+
+	tuples := make([]tuple, 0, len(data))
+	for _, i := range data {
+		tuples = append(tuples, tuple{key.NewKey(i.value.ID().Bytes()), i.value})
+	}
+	slices.SortFunc(tuples, func(a, b tuple) int {
+		return strings.Compare(a.key.String(), b.key.String())
+	})
+	want := make([]entity.RequestLog, 0, len(data))
+	for _, t := range tuples {
+		want = append(want, *t.value)
+	}
+	return want
 }
